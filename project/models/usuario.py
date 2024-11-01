04a1c05167ceded67e50cbaf05545129c50cc4b9
Moonghosty <128601188+Moonghosty@users.noreply.github.com>
2024-11-01 19:23:14 -0300

    Add files via upload

--- project/models/usuario.py
+++ project/models/usuario.py
@@ -5,6 +5,7 @@ class Usuario:
         self.senha = senha
 
 lista_usuarios=[]
-
-lista_usuarios.append(user= Usuario('1', "igor", "1234"))
-lista_usuarios.append(admin=Usuario('2', "admin", "admin"))
+user= Usuario(1, "igor", "1234")
+admin= Usuario(2, "admin", "admin")
+lista_usuarios.append(user)
+lista_usuarios.append(admin)
